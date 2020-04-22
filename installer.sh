73db8934e93d05ea760275b29871c49fffeb0a7f
Connor Walker <35660805+ConnorJWalker@users.noreply.github.com>
2020-04-22 15:50:31 +0100

    Create installer.sh

---
+++ installer.sh
@@ -0,0 +1,20 @@
+mkdir TSEG24_Project
+cd TSEG24_Project
+
+echo "Downloading robot code ..."
+
+git clone https://github.com/TSEG24/gamemotion.git
+
+echo "installing dependencies ..."
+
+pip3 install qibullet numpy opencv-python
+
+echo "Downloading vision code ..."
+
+git clone https://github.com/TSEG24/TSE-Vision.git
+
+echo "Installing vision code ..."
+
+cd TSE-Vision
+cmake .
+cmake --build .
